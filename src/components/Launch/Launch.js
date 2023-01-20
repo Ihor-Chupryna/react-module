76746f375a8f833394909e8ed441657d87c82954
Ihor-Chupryna <igchup1985@gmail.com>
2023-01-20 18:55:26 +0200

    lesson-2 part2

--- src/components/Launch/Launch.js
+++ src/components/Launch/Launch.js
@@ -1,11 +1,11 @@
-import './LaunchStyle.css';
+import './Launch.css';
 
 const Launch = ({launch}) => {
     return (
         <div className={'launch-wrapper'}>
             <h2>{launch.mission_name}</h2>
             <h3>launch year: {launch.launch_year}</h3>
-            <img src={launch.links.mission_patch_small} alt="photo"/>
+            <img src={launch.links.mission_patch_small} alt="photo mission"/>
         </div>
     );
 };
